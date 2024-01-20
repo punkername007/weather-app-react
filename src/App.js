a713e5e9e9a03487a260ca35722706f109dbf649
Selene Roa <152019449+punkername007@users.noreply.github.com>
2024-01-21 00:45:18 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <footer>
+        This project was coded by{" "}
+        <a href="#" target="_blank">
+          Selene Roa
+        </a>{" "}
+        and is{" "}
+        <a href="#" target="_blank">
+          open-sourced on GitHub
+        </a>{" "}
+        and{" "}
+        <a href="#" target="_blank">
+          hosted on Netlify
+        </a>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
