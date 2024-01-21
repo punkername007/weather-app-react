925e6dc9c629271583ef04abf5ee9ea9273327f9
Selene Roa <152019449+punkername007@users.noreply.github.com>
2024-01-21 01:46:10 +0100

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,32 +3,34 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://github.com/punkername007"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Selene Roa
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/punkername007/weather-app-react"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>{" "}
-        and{" "}
-        <a
-          href="https://sr-weather-react.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          hosted on Netlify
-        </a>
-      </footer>
+      <div className="container">
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://github.com/punkername007"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Selene Roa
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/punkername007/weather-app-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://sr-weather-react.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
